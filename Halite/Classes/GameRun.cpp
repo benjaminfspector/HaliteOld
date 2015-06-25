f75615b967efaa282400b29bf6516b6df186fa96
NuclearPi <benjamin_spector@horacemann.org>
2015-06-24 21:11:28 -0400

    j

--- Halite/Classes/GameRun.cpp
+++ Halite/Classes/GameRun.cpp
@@ -5,14 +5,9 @@ using namespace std;
 void runPlayer(short playerToRun);
 
 //Here are the player objects
-<<<<<<< HEAD
 //DiffusionAI Melissa;
 YOURNAME Bob;
-STD_AI_1 Alice;// , Jim, Fred, Tim;
-=======
-
-DiffusionAI Bob, Alice, Jim, Fred, Tim;
->>>>>>> origin/master
+STD_AI_1 Alice;
 //Sam, John, Kate, Sara, Melissa;
 //End here
 
